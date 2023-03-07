16968bbc3ce5fb8981efbf0e902b0b725857def4
Shamel <shamelatef@aucegypt.edu>
2023-03-07 23:42:39 +0200

    third

--- simple_facerec.py
+++ simple_facerec.py
@@ -3,30 +3,6 @@ import cv2
 import os
 import glob
 import numpy as np
-import time
-
-
-
-# load the pre-trained age classification model
-
-def faceBox(faceNet,frame):
-    frameHeight=frame.shape[0]
-    frameWidth=frame.shape[1]
-    blob=cv2.dnn.blobFromImage(frame, 1.0, (300,300), [104,117,123], swapRB=False)
-    faceNet.setInput(blob)
-    detection=faceNet.forward()
-    bboxs=[]
-    for i in range(detection.shape[2]):
-        confidence=detection[0,0,i,2]
-        if confidence>0.7:
-            x1=int(detection[0,0,i,3]*frameWidth)
-            y1=int(detection[0,0,i,4]*frameHeight)
-            x2=int(detection[0,0,i,5]*frameWidth)
-            y2=int(detection[0,0,i,6]*frameHeight)
-            bboxs.append([x1,y1,x2,y2])
-            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
-    return frame, bboxs
-
 
 class SimpleFacerec:
     def __init__(self):
@@ -35,17 +11,6 @@ class SimpleFacerec:
 
         # Resize frame for a faster speed
         self.frame_resizing = 0.25
-
-        # Load face detection and age detection models
-        self.faceProto = "opencv_face_detector.pbtxt"
-        self.faceModel = "opencv_face_detector_uint8.pb"
-        self.ageProto = "age_deploy.prototxt"
-        self.ageModel = "age_net.caffemodel"
-        self.faceNet = cv2.dnn.readNet(self.faceModel, self.faceProto)
-        self.ageNet = cv2.dnn.readNet(self.ageModel, self.ageProto)
-        self.MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
-        self.ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-        self.genderList = ['Male', 'Female']
 
     def load_encoding_images(self, images_path):
         """
@@ -74,8 +39,6 @@ class SimpleFacerec:
             self.known_face_names.append(filename)
         print("Encoding images loaded")
 
- 
-
     def detect_known_faces(self, frame):
         small_frame = cv2.resize(frame, (0, 0), fx=self.frame_resizing, fy=self.frame_resizing)
         # Find all the faces and face encodings in the current frame of video
@@ -85,45 +48,26 @@ class SimpleFacerec:
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
-        age_genders = []
-        for face_encoding, face_location in zip(face_encodings, face_locations):
+        for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
             name = "Unknown"
+
+            # # If a match was found in known_face_encodings, just use the first one.
+            # if True in matches:
+            #     first_match_index = matches.index(True)
+            #     name = known_face_names[first_match_index]
 
             # Or instead, use the known face with the smallest distance to the new face
             face_distances = face_recognition.face_distance(self.known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = self.known_face_names[best_match_index]
+            if name=='basbosa':
+                print("babsosa")
+            face_names.append(name)
 
-            # Detect age and gender of the face
-            y1, x2, y2, x1 = face_location
-            face = frame[y1:y2, x1:x2]
-            blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), self.MODEL_MEAN_VALUES, swapRB=False)
-            self.ageNet.setInput(blob)
-            agePreds=self.ageNet.forward()
-            age = self.ageList[agePreds[0].argmax()]
-            #gender = .genderList[self.c3er(face)]
-       
-
-            # Add the name to the list of names
-         
-            if name=='Unknown':
-                if age == '(0-2)':
-                    age = 'baby'
-                else:
-                    age = 'not baby'
-            else:
-                age='' 
-
-            # Draw a box around the face
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
-            cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-            cv2.putText(frame, age, (x1, y2 + 25), cv2.FONT_HERSHEY_DUPLEX, 0.8, (0, 0, 200), 2)
-
-        return frame, face_names, age_genders
-
-
-
-    
+        # Convert to numpy array to adjust coordinates with frame resizing quickly
+        face_locations = np.array(face_locations)
+        face_locations = face_locations / self.frame_resizing
+        return face_locations.astype(int), face_names
