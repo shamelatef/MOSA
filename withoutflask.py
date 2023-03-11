d06644aa2ee2d65e02c9c4656da4e54970285611
Shamel <shamelatef@aucegypt.edu>
2023-03-11 17:02:49 +0200

    hehe

--- withoutflask.py
+++ withoutflask.py
@@ -2,7 +2,7 @@ import cv2
 from simple_facerec import SimpleFacerec
 import time
 
-
+ 
 
 def faceBox(faceNet,frame):
     frameHeight=frame.shape[0]
@@ -34,6 +34,13 @@ ageModel = "age_net.caffemodel"
 faceNet=cv2.dnn.readNet(faceModel, faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
 
+faceNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+faceNet.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
+
+ageNet.setPreferableBackend(cv2.dnn.DNN_BACKEND_OPENCV)
+ageNet.setPreferableTarget(cv2.dnn.DNN_TARGET_OPENCL)
+
+
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
 genderList = ['Male', 'Female']
@@ -50,7 +57,8 @@ sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
 
 padding=50
-
+video.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+video.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
  # generate frames for web display
 while True:
         success, frame =video.read()
@@ -62,14 +70,6 @@ while True:
             face_locations, face_names = sfr.detect_known_faces(frame)
             frame,bboxs=faceBox(faceNet,frame)
  
-        
-
-            for face_loc, name in zip(face_locations, face_names):
-                        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-
-                        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
-
 
             for bbox in bboxs:
                             face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
@@ -98,8 +98,14 @@ while True:
 
                             cv2.rectangle(frame,(bbox[0], bbox[1]-30), (bbox[2], bbox[1]), (0,255,0),-1) 
                             cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
-            cv2.imshow("Age",frame)
 
+            for face_loc, name in zip(face_locations, face_names):
+                        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+                        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+
+            cv2.imshow("Age",frame)
             k=cv2.waitKey(1)
             if k==ord('q'):
                 break
