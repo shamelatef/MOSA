cdfc80dca33b499eb7d33a3888c2b1b633f21ef1
Shamel <shamelatef@aucegypt.edu>
2023-03-18 16:20:36 +0200

    first commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ port = 80
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect to the ESP8266
-s.connect((ip, port))
+#s.connect((ip, port))
 
 # Send a signal to turn on the LED
 
@@ -36,7 +36,7 @@ def faceBox(faceNet,frame):
             x2=int(detection[0,0,i,5]*frameWidth)
             y2=int(detection[0,0,i,6]*frameHeight)
             bboxs.append([x1,y1,x2,y2])
-            cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
+            #cv2.rectangle(frame, (x1,y1),(x2,y2),(0,255,0), 1)
     return frame, bboxs
 
 
@@ -53,34 +53,66 @@ ageNet=cv2.dnn.readNet(ageModel,ageProto)
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList = ['Male', 'Female']
 
-
-video=cv2.VideoCapture(0)
+video=cv2.VideoCapture(0) # The IP address of the esp32 CAM
 padding =20
-
-baby_detected = False
-baby_detected_start_time = 0
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
 sfr.load_encoding_images("images/")
-previous_name= ''
 
 
 while True:
-        #s.send(b'0')
         
         success, frame =video.read()
 
         if not success:
             break
         else:
+            start_time = time.time()
+
             # Detect Faces
+
             face_locations, face_names = sfr.detect_known_faces(frame)
             frame,bboxs=faceBox(faceNet,frame)
- 
+            for bbox,face_loc, name in zip(bboxs,face_locations, face_names):
+                    face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
+                    blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
 
+                    # Check if the face is known or unknown
+                    if name != "Unknown":
+                        # Send signal to the ESP8266
+                        if name == "shamel" or name == "amr" or name == "Mestekawy":
+                            #s.send(b'1')
+                            #time.sleep(1)
+                            print("Known")
+                        #else:
+                            #s.send(b'0')
+                    else:
+                        # Perform age detection
+                        ageNet.setInput(blob)
+                        agePred=ageNet.forward()
+                        age = agePred[0].argmax()
+
+                        # Send signal to the ESP8266 if the age group is a baby
+                        if age == 0:
+                            #s.send(b'0')
+                            print("baby")
+                        else:
+                            #s.send(b'1')
+                            print("notbaby")
+
+                    # Draw the bounding box and label on the frame
+                    y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+                    cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+                    end_time = time.time()
+            fps = 1 / (end_time - start_time)
+            cv2.putText(frame, f"FPS: {round(fps, 2)}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                    
+
+ 
+            """
             for bbox,face_loc, name in zip(bboxs,face_locations, face_names):
                             #print (name)
                             face = frame[max(0,bbox[1]-padding):min(bbox[3]+padding,frame.shape[0]-1),max(0,bbox[0]-padding):min(bbox[2]+padding, frame.shape[1]-1)]
@@ -104,13 +136,12 @@ while True:
 
                             previous_name = name                                
 
-                            y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
-                            
-                            
+                            y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]                          
                             cv2.rectangle(frame,(bbox[0], bbox[1]-30), (bbox[2], bbox[1]), (0,255,0),-1) 
                             #cv2.putText(frame, label, (bbox[0], bbox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255,255,255), 2,cv2.LINE_AA)
                             cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
                             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+                            """
 
             cv2.imshow("Frame",frame)
             k=cv2.waitKey(1)
